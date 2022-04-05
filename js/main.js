065fdcbf25f4c1523a3485f93aa83bca97fa431a
alxsm90@naver.com <alxsm90@naver.com>
2022-04-05 15:27:42 +0900

    배열 생성

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let menuCoffee = [ //커피메뉴
+    {name: '아메리카노', price: 3000},
+    {name: '아이스 아메리카노', price: 3500},
+    {name: '카페라떼', price: 4000},
+    {name: '아이스 카페라떼', price: 4500},
+    {name: '바닐라빈라떼', price: 4500},
+    {name: '아이스 바닐라빈라떼', price: 5000},
+    {name: '캐러멜라떼', price: 4500},
+    {name: '아이스 캐러멜라떼', price: 5000},
+    {name: '카페모카', price: 5000}
+]
+
+let menuTea = [ //티 메뉴
+    {name: '얼그레이', price: 3000},
+    {name: '아이스 얼그레이', price: 3500},
+    {name: '잉글리쉬브랙퍼스트', price: 3000},
+    {name: '아이스 잉글리쉬브랙퍼스트', price: 3500},
+    {name: '애플티', price: 3000},
+    {name: '아이스 애플티', price: 3500},
+    {name: '캐모마일', price: 3000},
+    {name: '아이스 캐모마일', price: 3500},
+    {name: '루이보스', price: 3000},
+    {name: '아이스 루이보스', price: 3500},
+    {name: '진저레몬', price: 3000},
+    {name: '아이스 진저레몬', price: 3500},
+    {name: '핫초콜릿', price: 3000},
+    {name: '아이스 초콜릿', price: 3500}
+]
+
+let menuSandwich = [
+    {name: '잠봉뵈르', price: 6000},
+    {name: '치킨클럽샌드위치', price: 6500},
+    {name: 'BLT샌드위치', price: 6500}
+]
