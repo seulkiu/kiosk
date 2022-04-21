12764d77c965f75caca45c0c8c8115ad97946bb8
alxsm90@naver.com <alxsm90@naver.com>
2022-04-21 17:38:05 +0900

    슬롬코드 복붙

--- js/main.js
+++ js/main.js
@@ -1,76 +1,182 @@
-
-
 let menuCoffee = [ //커피메뉴
-    {name: '아메리카노', price: 3000, 수량: 1},
-    {name: '아이스 아메리카노', price: 3500, 수량: 1},
-    {name: '카페라떼', price: 4000, 수량: 1},
-    {name: '아이스 카페라떼', price: 4500, 수량: 1},
-    {name: '바닐라빈라떼', price: 4500, 수량: 1},
-    {name: '아이스 바닐라빈라떼', price: 5000, 수량: 1},
-    {name: '캐러멜라떼', price: 4500, 수량: 1},
-    {name: '아이스 캐러멜라떼', price: 5000, 수량: 1},
-    {name: '카페모카', price: 4500, 수량: 1},
-    {name: '아이스 카페모카', price: 5000, 수량: 1}
+    {
+        name: '아메리카노',
+        price: 3000,
+        수량: 1
+    },
+    {
+        name: '아이스 아메리카노',
+        price: 3500,
+        수량: 1
+    },
+    {
+        name: '카페라떼',
+        price: 4000,
+        수량: 1
+    },
+    {
+        name: '아이스 카페라떼',
+        price: 4500,
+        수량: 1
+    },
+    {
+        name: '바닐라빈라떼',
+        price: 4500,
+        수량: 1
+    },
+    {
+        name: '아이스 바닐라빈라떼',
+        price: 5000,
+        수량: 1
+    },
+    {
+        name: '캐러멜라떼',
+        price: 4500,
+        수량: 1
+    },
+    {
+        name: '아이스 캐러멜라떼',
+        price: 5000,
+        수량: 1
+    },
+    {
+        name: '카페모카',
+        price: 4500,
+        수량: 1
+    },
+    {
+        name: '아이스 카페모카',
+        price: 5000,
+        수량: 1
+    }
 ]
 
 let menuTea = [ //티 메뉴
-    {name: '얼그레이', price: 3000, 수량: 1},
-    {name: '아이스 얼그레이', price: 3500, 수량: 1},
-    {name: '잉글리쉬브랙퍼스트', price: 3000, 수량: 1},
-    {name: '아이스 잉글리쉬브랙퍼스트', price: 3500, 수량: 1},
-    {name: '애플티', price: 3000, 수량: 1},
-    {name: '아이스 애플티', price: 3500, 수량: 1},
-    {name: '캐모마일', price: 3000, 수량: 1},
-    {name: '아이스 캐모마일', price: 3500, 수량: 1},
-    {name: '루이보스', price: 3000, 수량: 1},
-    {name: '아이스 루이보스', price: 3500, 수량: 1},
-    {name: '진저레몬', price: 3000, 수량: 1},
-    {name: '아이스 진저레몬', price: 3500, 수량: 1},
-    {name: '핫초콜릿', price: 3000, 수량: 1},
-    {name: '아이스 초콜릿', price: 3500, 수량: 1}
+    {
+        name: '얼그레이',
+        price: 3000,
+        수량: 1
+    },
+    {
+        name: '아이스 얼그레이',
+        price: 3500,
+        수량: 1
+    },
+    {
+        name: '잉글리쉬브랙퍼스트',
+        price: 3000,
+        수량: 1
+    },
+    {
+        name: '아이스 잉글리쉬브랙퍼스트',
+        price: 3500,
+        수량: 1
+    },
+    {
+        name: '애플티',
+        price: 3000,
+        수량: 1
+    },
+    {
+        name: '아이스 애플티',
+        price: 3500,
+        수량: 1
+    },
+    {
+        name: '캐모마일',
+        price: 3000,
+        수량: 1
+    },
+    {
+        name: '아이스 캐모마일',
+        price: 3500,
+        수량: 1
+    },
+    {
+        name: '루이보스',
+        price: 3000,
+        수량: 1
+    },
+    {
+        name: '아이스 루이보스',
+        price: 3500,
+        수량: 1
+    },
+    {
+        name: '진저레몬',
+        price: 3000,
+        수량: 1
+    },
+    {
+        name: '아이스 진저레몬',
+        price: 3500,
+        수량: 1
+    },
+    {
+        name: '핫초콜릿',
+        price: 3000,
+        수량: 1
+    },
+    {
+        name: '아이스 초콜릿',
+        price: 3500,
+        수량: 1
+    }
 ]
 
-let menuSandwich = [
-    {name: '잠봉뵈르', price: 6000, 수량: 1},
-    {name: '치킨클럽샌드위치', price: 6500, 수량: 1},
-    {name: 'BLT샌드위치', price: 6500, 수량: 1}
+let menuSandwich = [{
+        name: '잠봉뵈르',
+        price: 6000,
+        수량: 1
+    },
+    {
+        name: '치킨클럽샌드위치',
+        price: 6500,
+        수량: 1
+    },
+    {
+        name: 'BLT샌드위치',
+        price: 6500,
+        수량: 1
+    }
 ]
 
 // 기본 커피메뉴 보여주기
 // 함수 만들기
 
-function menuDraw(menu){
+function menuDraw(menu) {
     let menuArea = document.getElementById('menu_area');
     while (menuArea.hasChildNodes()) {
-        menuArea.removeChild(menuArea.firstChild);}
+        menuArea.removeChild(menuArea.firstChild);
+    }
 
     for (const key in menu) {
-    let li = document.createElement('li');
-    li.className = 'menu';
-    let menuName = document.createElement('p');
-    menuName.className = 'menu_name';
-    menuName.innerText = menu[key].name;
-    li.appendChild(menuName);
+        let li = document.createElement('li');
+        li.className = 'menu';
+        let menuName = document.createElement('p');
+        menuName.className = 'menu_name';
+        menuName.innerText = menu[key].name;
+        li.appendChild(menuName);
 
-    let price = document.createElement('p');
-    price.className = 'menu_price';
-    price.innerText = menu[key].price;
-    li.appendChild(price)
+        let price = document.createElement('p');
+        price.className = 'menu_price';
+        price.innerText = menu[key].price;
+        li.appendChild(price)
 
-    document.getElementById('menu_area').appendChild(li);
+        document.getElementById('menu_area').appendChild(li);
     }
 }
 
 // coffee, tea, sandwich 탭 누르면 해당 리스트 보여주기
 const selectTab = (tabNum) => {
-    switch(tabNum) {
-        case 1 :
+    switch (tabNum) {
+        case 1:
             menuDraw(menuCoffee);
             break;
-        case 2 :
+        case 2:
             menuDraw(menuTea);
             break;
-        case 3 :
+        case 3:
             menuDraw(menuSandwich);
             break;
     }
@@ -81,13 +187,63 @@ window.onload = () => {
     menuDraw(menuCoffee);
 };
 
-//토탈 상품 갯수, 토탈 금액 리프레시
+
+
+
+
+// 슬롬꺼 따라해보기
+let menu = [];
+let orderMenu = [];
+let totalCount = 0;
+let totalPrice = 0;
+//토탈 상품 갯수, 토탈 금액 리프레시 이게 뭘까요? 슬롬꺼 따라치는중인데 잘 모르겠음.
 const orderInfoRefresh = () => {
     totalCount = 0;
     totalPrice = 0;
     orderMenu.forEach((e) => {
-        totalCount += e.count;
+        totalCount += e.count; // totalCount = totalCount + e.count
         totalPrice += e.price * e.count;
     });
-    document.getElementById
+    document.getElementById('totalQuantitySpan').innerText = totalCount;
+    document.getElementById('totalPriceSpan').innerText = `${totalPrice.toLocalString()}원`;
+}
+
+// 선택한 메뉴를 화면에 뿌림
+const viewSelectMenu = (menu) => {
+    let cartArea = document.getElementById('cart_area');
+    let html = `<div class="cart_item" id="cart_item_${menu.id}">
+    <div class="top">
+        <span${menu.name}</span>
+        <button type="button" class="delete" onclick="orderDelete(${menu.id})">X</button>
+    </div>
+    <div class="middle">
+        <span>수량</span>
+        <div class="wrap__quantity">
+            <button type="button" class="plus" onclick="add(${menu.id})">+</button>
+            <span class="item_quantity" id="order_${menu.id}>${menu.count}">1</span>
+            <button type="button" class="minus" onclick="minus(${menu.id})">-</button>
+        </div>
+    </div>
+    <div class="bottom">
+        <span>가격</span>
+        <span id="order_price_${menu.id}">${menu.price.toLocalString()}원</span>>
+    </div>
+</div>`;
+    cartArea.insertAdjacentHTML("beforeend", html);
+}
+
+// 메뉴에서 선책
+const selectMenu = (id) => {
+    let selectMenu = menu.find((e) => e.id == id);
+    let orderMenuItem = orderMenu.find((e) => e.id == id);
+    if (orderMenuItem == null) {
+        selectMenu.count = 1;
+        orderMenu.push(selectMenu);
+        viewSelectMenu(selectMenu);
+    } else {
+        orderMenuItem.count++;
+        document.getElementById(`order_${id}`).innerText = orderMenuItem.count;
+        document.getElementById(`order_price_${id}`).innerText = `${(orderMenuItem.price * orderMenuItem.count).toLocalString()}원`;
+    }
+    orderInfoRefresh();
 }
