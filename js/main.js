9b95e52c500e6c181576e4e5e16c9e7555857c49
alxsm90@naver.com <alxsm90@naver.com>
2022-04-22 14:11:50 +0900

    count 속성 수정

--- js/main.js
+++ js/main.js
@@ -149,34 +149,43 @@ function menuDraw(menu) {
     while (menuArea.hasChildNodes()) {
         menuArea.removeChild(menuArea.firstChild);
     }
+    // 메뉴를 화면에 그려줌
+    menu.forEach((e) => {
+        let html = `<li class="menu" onclick="selectMenu(${e.id})"><p class="menu_name"><span>${e.name}</span></p><p class="menu_price">${e.price.toLocaleString()}원</p></li>`;
+        menuArea.insertAdjacentHTML("beforeend", html);
+    });
 
-    for (const key in menu) {
-        let li = document.createElement('li');
-        li.className = 'menu';
-        let menuName = document.createElement('p');
-        menuName.className = 'menu_name';
-        menuName.innerText = menu[key].name;
-        li.appendChild(menuName);
+    // for (const key in menu) {
+    //     let li = document.createElement('li');
+    //     li.className = 'menu';
+    //     let menuName = document.createElement('p');
+    //     menuName.className = 'menu_name';
+    //     menuName.innerText = menu[key].name;
+    //     li.appendChild(menuName);
 
-        let price = document.createElement('p');
-        price.className = 'menu_price';
-        price.innerText = menu[key].price;
-        li.appendChild(price)
+    //     let price = document.createElement('p');
+    //     price.className = 'menu_price';
+    //     price.innerText = menu[key].price;
+    //     li.appendChild(price)
 
-        document.getElementById('menu_area').appendChild(li);
-    }
+    //     document.getElementById('menu_area').appendChild(li);
+    // }
 }
 
 // coffee, tea, sandwich 탭 누르면 해당 리스트 보여주기
+let tab = 0;
 const selectTab = (tabNum) => {
     switch (tabNum) {
         case 1:
+            tab = tabNum;
             menuDraw(menuCoffee);
             break;
         case 2:
+            tab = tabNum;
             menuDraw(menuTea);
             break;
         case 3:
+            tab = tabNum;
             menuDraw(menuSandwich);
             break;
     }
@@ -184,6 +193,7 @@ const selectTab = (tabNum) => {
 
 
 window.onload = () => {
+    tab = 1;
     menuDraw(menuCoffee);
 };
 
@@ -192,16 +202,69 @@ window.onload = () => {
 
 
 // 슬롬꺼 따라해보기
+let orderMenu = [];
+let totalCount = 0;
+let totalPrice = 0;
 
 
-//
-const goPaymentPage = () => {
-    // 결제 페이지로 이동할 때  order리스트를 보관하기 위래 로컬스토리지 사용
-    if (orderMenu.length === 0) {
-        alert('메뉴가 선택되지 않았습니다.');
-        return;
-    }
-    localStorage.setItem('orderMenu', JSON.stringify(orderMenu));
-    //결제 페이지 이동
-    location.href = 'payment.html';
+
+// 토탈 상품 갯수, 토탈 금액 리프레시
+const orderInfoRefresh = () => {
+    totalCount = 0;
+    totalPrice = 0;
+    orderMenu.forEach((e) => {
+        totalCount += e.count;
+        totalPrice += e.price * e.count;
+    });
+    document.getElementById('totalQuantitySpan').innerText = totalCount;
+    document.getElementById('totalPriceSpan').innerText = `${totalPrice.toLocaleString()}원`;
 }
+
+
+//  메뉴에서 선택
+const selectMenu = (id) => {
+    let selectMenu;
+    if (tab === 1) {
+        selectMenu = menuCoffee.find((e) => e.id === id);
+    } else if (tab === 2) {
+        selectMenu = menuTea.find((e) => e.id === id);
+    } else {
+        selectMenu = menuSandwich.find((e) => e.id === id);
+    }
+    let orderMenuItem = orderMenu.find((e) => e.id === id);
+    if (orderMenuItem === undefined) {
+        selectMenu['count'] = 1;
+        orderMenu.push(selectMenu);
+        viewSelectMenu(selectMenu); 
+    } else {
+        orderMenuItem.count++;
+        document.getElementById(`order_${id}`).innerText = orderMenuItem.count;
+        document.getElementById(`order_price_${id}`).innerText = `${(orderMenuItem.price * orderMenuItem.count).toLocaleString()}원`;
+    }
+    orderInfoRefresh();
+}
+
+// 선택한 메뉴를 화면에 뿌림
+const viewSelectMenu = (menu) => {
+    let cartArea = document.getElementById('cart_area');
+    let html = `<div class="cart_item" id="cart_item_${menu.id}">
+    <div class="top">
+        <span>${menu.name}</span>
+        <button type="button" class="delete" onclick="orderDelete(${menu.id})">X</button>
+    </div>
+    <div class="middle">
+        <span>수량</span>
+        <div class="wrap__quantity">
+            <button type="button" class="plus" onclick="add(${menu.id})">+</button>
+            <span class="item_quantity" id="order_${menu.id}">${menu.count}</span>
+            <button type="button" class="minus" onclick="minus(${menu.id})">-</button>
+        </div>
+    </div>
+    <div class="bottom">
+        <span>가격</span>
+        <span id="order_price_${menu.id}">${menu.price.toLocaleString()}원</span>
+    </div>
+</div>`;
+cartArea.insertAdjacentHTML("beforeend", html);
+}
+
