fb519a8e59ee0f6c8207f30a26edd29d693e0f1b
alxsm90@naver.com <alxsm90@naver.com>
2022-04-22 09:39:15 +0900

    선택메뉴 alert

--- js/main.js
+++ js/main.js
@@ -192,58 +192,16 @@ window.onload = () => {
 
 
 // 슬롬꺼 따라해보기
-let menu = [];
-let orderMenu = [];
-let totalCount = 0;
-let totalPrice = 0;
-//토탈 상품 갯수, 토탈 금액 리프레시 이게 뭘까요? 슬롬꺼 따라치는중인데 잘 모르겠음.
-const orderInfoRefresh = () => {
-    totalCount = 0;
-    totalPrice = 0;
-    orderMenu.forEach((e) => {
-        totalCount += e.count; // totalCount = totalCount + e.count
-        totalPrice += e.price * e.count;
-    });
-    document.getElementById('totalQuantitySpan').innerText = totalCount;
-    document.getElementById('totalPriceSpan').innerText = `${totalPrice.toLocalString()}원`;
-}
 
-// 선택한 메뉴를 화면에 뿌림
-const viewSelectMenu = (menu) => {
-    let cartArea = document.getElementById('cart_area');
-    let html = `<div class="cart_item" id="cart_item_${menu.id}">
-    <div class="top">
-        <span${menu.name}</span>
-        <button type="button" class="delete" onclick="orderDelete(${menu.id})">X</button>
-    </div>
-    <div class="middle">
-        <span>수량</span>
-        <div class="wrap__quantity">
-            <button type="button" class="plus" onclick="add(${menu.id})">+</button>
-            <span class="item_quantity" id="order_${menu.id}>${menu.count}">1</span>
-            <button type="button" class="minus" onclick="minus(${menu.id})">-</button>
-        </div>
-    </div>
-    <div class="bottom">
-        <span>가격</span>
-        <span id="order_price_${menu.id}">${menu.price.toLocalString()}원</span>>
-    </div>
-</div>`;
-    cartArea.insertAdjacentHTML("beforeend", html);
-}
 
-// 메뉴에서 선책
-const selectMenu = (id) => {
-    let selectMenu = menu.find((e) => e.id == id);
-    let orderMenuItem = orderMenu.find((e) => e.id == id);
-    if (orderMenuItem == null) {
-        selectMenu.count = 1;
-        orderMenu.push(selectMenu);
-        viewSelectMenu(selectMenu);
-    } else {
-        orderMenuItem.count++;
-        document.getElementById(`order_${id}`).innerText = orderMenuItem.count;
-        document.getElementById(`order_price_${id}`).innerText = `${(orderMenuItem.price * orderMenuItem.count).toLocalString()}원`;
+//
+const goPaymentPage = () => {
+    // 결제 페이지로 이동할 때  order리스트를 보관하기 위래 로컬스토리지 사용
+    if (orderMenu.length === 0) {
+        alert('메뉴가 선택되지 않았습니다.');
+        return;
     }
-    orderInfoRefresh();
+    localStorage.setItem('orderMenu', JSON.stringify(orderMenu));
+    //결제 페이지 이동
+    location.href = 'payment.html';
 }
